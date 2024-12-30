b4ff255f74aa32942db322a362fc4c15b60bccd5
Jose Jesus <cp.josejesus@gmail.com>
2024-12-29 20:00:46 -0400

    structure the project

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+function Nav(props) {
+  return (
+    <nav className="nav">
+      <img className="nav__logo" src="" alt="" />
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
